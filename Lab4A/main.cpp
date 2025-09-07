4351d5092cd38cce14d31e640dd82181ea79ee92
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-07 00:17:48 -0700

    finished lab4a with formatted table

--- Lab4A/main.cpp
+++ Lab4A/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
 #include <random>
 
 
@@ -19,8 +20,30 @@ int main (){
     uniform_int_distribution<int> pickRGB(0, 255);
 
     int n = pickN(gen);
-    int rgb = pickRGB(gen);
-    cout << "n = " << n << "rgb = "<< rgb <<endl;
+
+    vector<Color> colors;
+
+    for(int i = 0; i < n; i++){
+        Color c;
+        c.red = pickRGB(gen);
+        c.green = pickRGB(gen);
+        c.blue = pickRGB(gen);
+        colors.push_back(c);
+    }
+
+    cout << left 
+    << setw(8) 
+    << "Color#" 
+    << setw(10) 
+    << "R"
+    << setw(10) 
+    << "G" 
+    << setw(10) 
+    << "B" << '\n';
+
+    for(size_t i = 0; i < colors.size(); i++){
+        cout<< left << setw(8) << i+1 << setw(10)<< colors[i].red << setw(10) << colors[i].green << setw(10) << colors[i].blue << endl;
+    }
 
     return 0;
 }
