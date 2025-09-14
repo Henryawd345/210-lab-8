bd99e1b41ae56776c8bcf58c106fe42a4f7fb895
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-14 02:49:07 -0700

    10 minutes update

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+void displayArray(const string* a, int n) {
+    // TODO: print all n strings on one line using pointer notation
+    // for (int i = 0; i < n; ++i) { cout << /* *(a+i) */ << ' '; }
+    // cout << '\n';
+
+    for (int i = 0; i < n; ++i) { 
+        cout << *(a+i) << ' '; 
+    }
+}
