9c471034756cf3942122cc51dd6bf3494b6d5b4c
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-14 03:00:47 -0700

    10 minutes update 2

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -2,12 +2,24 @@
 #include <string>
 using namespace std;
 
-void displayArray(const string* a, int n) {
-    // TODO: print all n strings on one line using pointer notation
-    // for (int i = 0; i < n; ++i) { cout << /* *(a+i) */ << ' '; }
-    // cout << '\n';
-
+void displayArray(string* a, int n) {
     for (int i = 0; i < n; ++i) { 
         cout << *(a+i) << ' '; 
     }
+}
+
+string* reverseArray(string* a, int n){
+    string* startarray = a;
+    string* endarray = a + (n - 1);
+
+    while (left < right){
+        string temp = *startarray;
+        *startarray = *endarray;
+        *endarray = temp;
+
+        startarray++;
+        endarray--;
+    }
+    
+    return a;
 }
