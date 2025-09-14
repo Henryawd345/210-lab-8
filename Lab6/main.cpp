b51b3b9ed1ff49b2332f152746c8e580ce73c889
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-14 01:54:11 -0700

    10 minutes update outputarray and sumarray create

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -10,4 +10,24 @@ void enterArrayData(double* a, int n) {
     cout << "Data entry complete.\n";
 }
 
+void outputArrayData(const double* a, int n) {
+    cout << "Outputting array elements: ";
+    for (int i = 0; i < n; i++){
+        cout << *(a + i)<< " ";
+    }
+    cout << '\n';
+}
 
+
+double sumArray(const double* a, int n) {
+    double sum = 0.0;
+    for (int i = 0; i < n; i++){
+        sum += *(a + i);
+    }
+    return sum;
+}
+
+int main(){
+    double sum = 0.0;                  // not initialized
+    cout << "Starts as: " << sum << '\n';  // you'll see it's not 0
+}
