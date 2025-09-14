a77fd1920d2ab03034331a1a71b6a2bd8d990a6f
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-14 01:38:26 -0700

    enterArrayData function

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+void enterArrayData(double* a, int n) {
+    cout << "Data entry for the array:\n";
+    for (int i = 0; i < n; i++){
+        cout << " > Element #" << i << ": ";
+        cin >> *(a + i);
+    }
+    cout << "Data entry complete.\n";
+}
+
+
